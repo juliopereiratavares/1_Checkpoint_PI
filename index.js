9f258630dda2c155fc436d095d218682438473e5
Julio Tavares <jptavares145@hotmail.com>
2022-03-20 18:19:53 -0300

    Envio do trabalho feito em grupo na sala de aula

---
+++ index.js
@@ -0,0 +1,21 @@
+let pedido = menu
+
+switch(pedido){
+case '1-pipoca':
+    console.log(10 + "segundos")
+    break;
+    case '2-macarrão':
+        console.log(8 + "segundos")
+    break;
+    case '3-Carne':
+        console.log(15 + "segundos")
+        break;
+    case '4- Feijão':
+        console.log(12 + "segundos")
+        break;
+        case '5- Brigadeiro':
+            console.log(8+ "segundos")
+            break;
+            default: 
+            console.log("Proto pronto ! Bom apetite ");
+}
